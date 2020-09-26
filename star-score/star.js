4b0a0b6ed547882beff2440dfaf2418f100c1d6c
chichi <jang.chihun@gmail.com>
2020-09-26 21:45:22 +0900

    [star-score] 별 반개 표시 기능 추가

--- star-score/star.js
+++ star-score/star.js
@@ -4,13 +4,29 @@ Array.from(STAR_ITEMS).map((item) =>
   item.addEventListener("click", markingStar)
 );
 
-function markingStar() {
-  console.log(this.id);
-  const click_star = this.id.substring(4);
+function markingStar(e) {
+  if (e.offsetX <= 50) {
+    console.log("왼쪽");
+  } else {
+    console.log("오른쪽");
+  }
+  console.log(e.offsetX, this);
+
+  const click_star = parseInt(this.id.substring(4));
   let idx = 1;
   let remain_idx;
   while (idx <= click_star) {
-    document.querySelector(`#star${idx}`).src = "full.png";
+    if (idx === click_star) {
+      if (e.offsetX <= 50) {
+        console.log("반");
+        document.querySelector(`#star${idx}`).src = "half.png";
+      } else {
+        console.log("풀");
+        document.querySelector(`#star${idx}`).src = "full.png";
+      }
+    } else {
+      document.querySelector(`#star${idx}`).src = "full.png";
+    }
     idx++;
     remain_idx = idx;
   }
